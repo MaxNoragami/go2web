f2c160c740411770621f117e142de7368f53ae4b
Maxim <76247935+MaxNoragami@users.noreply.github.com>
2026-03-29 18:57:02 +0300

    feat: add support for base commands
    * chore: switch to ConsoleAppFramework from Spectre.Console.Cli
    
    * feat: add base commands structure

--- Program.cs
+++ Program.cs
@@ -1,30 +1,6 @@
-﻿using System.ComponentModel;
-using Spectre.Console;
-using Spectre.Console.Cli;
+﻿using go2web.Commands;
+using ConsoleAppFramework;
 
-var app = new CommandApp<GreetCommand>();
-return app.Run(args);
-
-public class GreetSettings : CommandSettings
-{
-    [CommandArgument(0, "<name>")]
-    [Description("The name to greet")]
-    public required string Name { get; init; }
-
-    [CommandOption("-c|--count")]
-    [Description("Number of times to greet")]
-    [DefaultValue(1)]
-    public int Count { get; init; }
-}
-
-public class GreetCommand : Command<GreetSettings>
-{
-    public override int Execute(CommandContext context, GreetSettings settings, CancellationToken cancellationToken)
-    {
-        for (var i = 0; i < settings.Count; i++)
-        {
-            AnsiConsole.MarkupLine($"Hello, [green]{settings.Name}[/]!");
-        }
-        return 0;
-    }
-}
+var app = ConsoleApp.Create();
+app.Add<Commands>();
+app.Run(args);
